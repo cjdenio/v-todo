c202b6d3ba4ecacc05a69edb1be1a93c07195b05
Caleb Denio <cjdenio44@gmail.com>
2021-02-18 21:01:47 -0500

    ✨ Item creation and code formatting

--- main.v
+++ main.v
@@ -1,8 +1,8 @@
 import pg
 import vweb
-
 import net.urllib
 import os { getenv }
+import rand { uuid_v4 }
 
 struct App {
 	vweb.Context
@@ -15,23 +15,23 @@ fn main() {
 }
 
 pub fn (mut app App) init_once() {
-	url := urllib.parse(getenv("DB_URL")) or { panic(err) }
+	url := urllib.parse(getenv('DB_URL')) or { panic(err) }
 
-	app.db = pg.connect(pg.Config {
-		host: url.hostname(),
-		port: url.port().int(),
-		user: url.user.username,
-		password: url.user.password,
+	app.db = pg.connect(pg.Config{
+		host: url.hostname()
+		port: url.port().int()
+		user: url.user.username
+		password: url.user.password
 		dbname: url.path[1..]
-	}) or {
+	}) or { panic(err) }
+
+	app.db.exec('create table if not exists items (id varchar primary key, text varchar)') or {
 		panic(err)
 	}
-
-	app.db.exec("create table if not exists items (id integer primary key, text varchar)") or { panic(err) }
 }
 
 fn (mut app App) index() vweb.Result {
-	rows := app.db.exec("select text from items") or { panic(err) }
+	rows := app.db.exec('select text from items') or { panic(err) }
 	items := rows.map(fn (row pg.Row) string {
 		return row.vals[0]
 	})
@@ -39,4 +39,18 @@ fn (mut app App) index() vweb.Result {
 	len := items.len
 
 	return $vweb.html()
+}
+
+[post]
+fn (mut app App) create() vweb.Result {
+	id := uuid_v4()
+
+	app.db.exec_param_many('insert into items (id, text) values ($1, $2)', [id, app.form['text']]) or {
+		panic(err)
+	}
+
+	println(app.form['text'])
+	println(app.form)
+
+	return app.redirect('/')
 }
